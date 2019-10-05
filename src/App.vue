527f0892763bf2f8e6b6d6c44f99a93629477f5b
Diego Torres <diegotorres010@hotmail.com>
2019-10-05 10:56:53 -0500

    assd

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,4 @@ export default {
   }
 };
 </script>
-<style>
-body{
-    background: url(https://www.toptal.com/designers/subtlepatterns/patterns/doodles.png) repeat 0 0;
-    width: 100%;
-    margin: 0;
-    text-align: center;
-    height: 100%;
-    /* padding-top: 120px; */
-    box-sizing: border-box;
-    -webkit-animation: slide 20s ease-in-out infinite;
-  }
-  
-  @-webkit-keyframes slide {
-      from { background-position: 0 0; }
-      to { background-position: -1000px 0; }
-  }
-</style>
 
